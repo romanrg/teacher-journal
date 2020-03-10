c99c266cd3f11bb189145856a27373e978560adf
Raman_Nekhaichyk <Raman_Nekhaichyk@epam.com>
2020-03-10 13:56:08 +0300

    refactor: add typedef for SUBJECT_HEADERS_LENGTH

--- src/app/common/constants/SUBJECT_HEADERS.ts
+++ src/app/common/constants/SUBJECT_HEADERS.ts
@@ -1,3 +1,3 @@
 export const SUBJECT_HEADERS: string[] = ["name", "surname", "average mark"];
 
-export const SUBJECT_HEADERS_LENGTH = SUBJECT_HEADERS.length;
+export const SUBJECT_HEADERS_LENGTH: number = SUBJECT_HEADERS.length;
