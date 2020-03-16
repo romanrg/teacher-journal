fde491a73a3949cac72dd9d851dbe5cdf13a72f8
Raman_Nekhaichyk <Raman_Nekhaichyk@epam.com>
2020-03-16 17:38:56 +0300

    refactor: change typedef of headers constant

--- src/app/common/constants/SUBJECT_HEADERS.ts
+++ src/app/common/constants/SUBJECT_HEADERS.ts
@@ -1,3 +1,3 @@
-export const SUBJECT_HEADERS: string[] = ["name", "surname", "average mark"];
+export const SUBJECT_HEADERS: ReadonlyArray<string> = ["name", "surname", "average mark"];
 
 export const SUBJECT_HEADERS_LENGTH: number = SUBJECT_HEADERS.length;
