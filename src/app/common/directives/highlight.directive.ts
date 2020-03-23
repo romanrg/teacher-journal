6c92257cbfaabfdf46d9b500010f6bab3bbd3b36
Raman_Nekhaichyk <Raman_Nekhaichyk@epam.com>
2020-03-23 11:56:47 +0300

    feat: fix layout

--- src/app/common/directives/highlight.directive.ts
+++ src/app/common/directives/highlight.directive.ts
@@ -10,6 +10,7 @@ export class HighlightDirective {
 
   private highlight(color: string): void {
     this.el.nativeElement.style.cursor = "pointer";
+
     if (color) {
       this.el.nativeElement.style.borderBottom = `solid 0.05rem ${color}`;
     } else {
